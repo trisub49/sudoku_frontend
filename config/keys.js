084f8623e2a9e03862932e761c07d2efc1cac45a
trisub49 <trisub49@gmail.com>
2021-06-07 17:22:33 +0200

    third try

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    google: {
+        clientID: '110966933052-nc2obhmjqjhjug4u7r7i6vi7elajmfdc.apps.googleusercontent.com',
+        clientSecret: 'ZgjE3PWq9hBtHymZvUgXR05z'
+    },
+    database: {
+        URL: 'mongodb+srv://trisub49:193jkgte7m@cluster0.lmwa5.mongodb.net/sudoku?retryWrites=true&w=majority'
+    },
+    cookieSession: {
+        key: 'alabada'
+    }
+}
